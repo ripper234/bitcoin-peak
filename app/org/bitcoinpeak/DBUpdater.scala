5c5d4b6a1c2fe6325bc057adb838da502104fba8
Ron Gross <ron.gross@gmail.com>
2013-06-14 03:43:38 +0300

    Add log

--- app/org/bitcoinpeak/DBUpdater.scala
+++ app/org/bitcoinpeak/DBUpdater.scala
@@ -1,9 +1,11 @@
 package org.bitcoinpeak
 
 import org.quartz.JobExecutionContext
+import play.api.Logger
 
 class DBUpdater extends org.quartz.Job {
   def execute(p1: JobExecutionContext) {
+    Logger.info("Updating database")
     val peak : BigDecimal = BitcoinChartsPeakPriceFinder.calcPeak()
     Peak.addPeak(peak)
   }
